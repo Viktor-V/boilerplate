d5ace2adb092a1a83e43a12d080ccab67dffd3eb
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2020-12-06 12:45:33 +0200

    Configured phpstan and some code style fixes

--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace App\Tests;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
 
 class SampleTest extends TestCase
 {
     public function testSample(): void
     {
-        $this->assertTrue(true);
+        Assert::assertTrue(true);
     }
 }
