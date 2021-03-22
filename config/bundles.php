d82fe66a8fc51686ebb06988280f801d75529b6a
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2021-03-22 20:47:20 +0200

    Removed roadrunner

--- config/bundles.php
+++ config/bundles.php
@@ -2,6 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Baldinof\RoadRunnerBundle\BaldinofRoadRunnerBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
