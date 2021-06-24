d07134a12de05f4ebe0af9bf92d94ab2061a5d68
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2021-06-24 09:38:00 +0300

    Speed up ci/cd

--- config/packages/test/framework.php
+++ config/packages/test/framework.php
@@ -7,6 +7,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->extension('framework', [
         'test' => true,
+        'assets' => ['json_manifest_path' => null],
         'session' => [
             'storage_factory_id' => 'session.storage.factory.mock_file'
         ]
