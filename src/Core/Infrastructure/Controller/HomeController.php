715dd9f20f8ddfe687754d7ad798c6c4fd51a98a
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2021-10-02 19:23:20 +0300

    fix

--- src/Core/Infrastructure/Controller/HomeController.php
+++ src/Core/Infrastructure/Controller/HomeController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Core\Infrastructure\Controller;
 
 use App\BaseKernel;
-use App\Core\CoreRouteName;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
