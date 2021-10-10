69902c3d8817e79264c2e09fce0280cfab48c6d8
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2021-10-10 16:15:07 +0300

    fix admin route. moved from admin_language.php

--- config/modules/routes/admin_cache.php
+++ config/modules/routes/admin_cache.php
@@ -4,8 +4,21 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use App\Admin\AdminCache\AdminCacheModule;
+use App\Core\Admin\Infrastructure\Controller\AbstractController;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routingConfigurator): void {
-    /* @see ./admin_core.php */
+    $module = new AdminCacheModule();
+
+    if (!$module->enable()) {
+        return;
+    }
+
+    $routingConfigurator
+        ->import(
+            __DIR__ . '/../../../src/admin/AdminCache/Infrastructure/Controller/',
+            'annotation'
+        )
+        ->prefix('/' . AbstractController::ADMIN_CORE_PATH);
 };
--- config/modules/routes/admin_language.php
+++ config/modules/routes/admin_language.php
@@ -4,38 +4,21 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use App\Admin\AdminCache\AdminCacheModule;
 use App\Core\Admin\Infrastructure\Controller\AbstractController;
-use App\Core\Module\Contract\AdminModuleInterface;
-use App\Core\Module\Contract\ModuleInterface;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routingConfigurator): void {
-    $contents = require __DIR__ . '/../../modules.php';
-    foreach ($contents as $class) {
-        /** @var ModuleInterface $module */
-        $module = new $class();
+    $module = new AdminCacheModule();
 
-        $isAdminModule = $module instanceof AdminModuleInterface;
-        if (!$isAdminModule) {
-            continue;
-        }
-
-        if ($module->enable()) {
-            try {
-                $routingConfigurator
-                    ->import(
-                        __DIR__ . '/../../../src/' . camelize($module->name()) . '/Infrastructure/Controller/',
-                        'annotation'
-                    )
-                    ->prefix('/' . AbstractController::ADMIN_CORE_PATH);
-            } catch (\Throwable $e) {
-                $routingConfigurator
-                    ->import(
-                        __DIR__ . '/../../../src/admin/' . camelize($module->name()) . '/Infrastructure/Controller/',
-                        'annotation'
-                    )
-                    ->prefix('/' . AbstractController::ADMIN_CORE_PATH);
-            }
-        }
+    if (!$module->enable()) {
+        return;
     }
+
+    $routingConfigurator
+        ->import(
+            __DIR__ . '/../../../src/admin/AdminLanguage/Infrastructure/Controller/',
+            'annotation'
+        )
+        ->prefix('/' . AbstractController::ADMIN_CORE_PATH);
 };
