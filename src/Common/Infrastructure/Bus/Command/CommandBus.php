eb6b71ade66f36eaa6a036b06925f6cb46e60b52
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2022-02-06 16:10:53 +0200

    Created simplest CQRS and domain event

---
+++ src/Common/Infrastructure/Bus/Command/CommandBus.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common\Infrastructure\Bus\Command;
+
+use App\Common\Application\Command\CommandBusInterface;
+use App\Common\Application\Command\CommandInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+final class CommandBus implements CommandBusInterface
+{
+    public function __construct(
+        private MessageBusInterface $commandBus
+    ) {}
+
+    public function dispatch(CommandInterface $command): void
+    {
+        $this->commandBus->dispatch($command);
+    }
+}
---
+++ src/Common/Infrastructure/Bus/Event/EventBus.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common\Infrastructure\Bus\Event;
+
+use App\Common\Domain\Event\EventBusInterface;
+use App\Common\Domain\Event\EventInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+final class EventBus implements EventBusInterface
+{
+    public function __construct(
+        private MessageBusInterface $commandBus
+    ) {}
+
+    public function dispatch(EventInterface $event): void
+    {
+        $this->commandBus->dispatch($event);
+    }
+}
