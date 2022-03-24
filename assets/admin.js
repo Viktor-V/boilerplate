ca28c0bac439f309cc20dbca6bb4f46c877199e1
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2022-03-24 18:09:27 +0200

    Added adminlte

---
+++ assets/admin.js
@@ -0,0 +1,21 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/admin.css';
+
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+import '@fortawesome/fontawesome-free/js/fontawesome'
+
+import 'jquery'
+import 'bootstrap'
+import 'admin-lte'
+
+// start the Stimulus application
+import './bootstrap';
