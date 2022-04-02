43f6278d0f0ca312f2b23e849619a8d6ecd9247a
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2022-04-02 10:56:42 +0300

    Removed project

--- src/Admin/Domain/Entity/ValueObject/Uuid.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Admin\Domain\Entity\ValueObject;
-
-final class Uuid extends \App\Common\Domain\ValueObject\Uuid
-{
-}
--- ui/http/Common/Payload/CommandPayloadInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace UI\Http\Common\Payload;
-
-interface CommandPayloadInterface extends PayloadInterface
-{
-}
